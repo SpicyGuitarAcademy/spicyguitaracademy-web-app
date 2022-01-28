15c22d8ae92053aba330cc8ee8cf0c3db8fff851
Ebuka Odini <ebukaodini@gmail.com>
2022-01-28 17:32:04 +0100

    feat: implemented the landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { LandingPage } from './pages/public';
 
 function App() {
   return (
-    <div className="App bg-light">
-      <header className="App-header text-dark">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code>, save and reload.
-        </p>
-        <a
-          className="text-primary"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Now
-        </a>
-      </header>
-    </div>
+    <LandingPage />
   );
 }
 
