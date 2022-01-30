9701514e98b1534d7d1f546b7c712d022449d86d
Ebuka Odini <ebukaodini@gmail.com>
2022-01-31 00:31:44 +0100

    feat: added the register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { LandingPage, LoadingPage, WelcomePage } from './pages/public';
+import { LandingPage, LoadingPage, RegisterPage, WelcomePage } from './pages/public';
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
         <Route exact path='/' children={LandingPage} />
         <Route exact path='/welcome' children={WelcomePage} />
         <Route exact path='/loading' children={LoadingPage} />
+        <Route exact path='/register' children={RegisterPage} />
       </Switch>
     </BrowserRouter>
   );
--- src/pages/public/index.tsx
+++ src/pages/public/index.tsx
@@ -1,3 +1,4 @@
 export { LandingPage } from './landing-page/landing-page'
 export { LoadingPage } from './loading-page/loading-page'
 export { WelcomePage } from './welcome-page/welcome-page'
+export { RegisterPage } from './register-page/register-page'
