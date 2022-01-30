8093a4e2b5b8451023aa37bd993ece712e648bbc
Ebuka Odini <ebukaodini@gmail.com>
2022-01-31 00:35:19 +0100

    refactor: fixed styling error

--- src/pages/public/welcome-page/welcome-page.tsx
+++ src/pages/public/welcome-page/welcome-page.tsx
@@ -4,7 +4,7 @@ import { PublicPageWrapper } from "../../../components"
 export const WelcomePage = () => {
   return (
     <PublicPageWrapper bgImage='__bg1'>
-      <div className='h-100 d-flex justify-content-center align-items-center my-lg-5'>
+      <div className='h-60 w-100 d-flex justify-content-center align-items-center'>
         <div className='d-flex flex-column justify-content-center align-items-center'>
           <p className='h1 text-cream mb-lg-5 fw-bold'>Hi, Welcome to Spicy Guitar Academy.</p>
           <div>
