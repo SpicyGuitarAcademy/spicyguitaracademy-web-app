e97c41b26febaabaa4e07ecd73b14414edc8f738
Ebuka Odini <ebukaodini@gmail.com>
2022-01-30 14:30:56 +0100

    feat: added the welcome page to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { LandingPage, LoadingPage } from './pages/public';
+import { LandingPage, LoadingPage, WelcomePage } from './pages/public';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/' children={LandingPage} />
+        <Route exact path='/welcome' children={WelcomePage} />
         <Route exact path='/loading' children={LoadingPage} />
       </Switch>
     </BrowserRouter>
