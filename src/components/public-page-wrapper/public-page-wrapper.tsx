b273afd25c664f4c2b19dd7c273e7d823053c921
Ebuka Odini <ebukaodini@gmail.com>
2022-01-31 00:50:48 +0100

    refactor: updated the wrapper to link back to the welcome page

--- src/components/public-page-wrapper/public-page-wrapper.tsx
+++ src/components/public-page-wrapper/public-page-wrapper.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import SpicyGuitarLogo from '../../assets/spicyguitaracademy_logo.svg';
 import './style.scss'
 
@@ -8,7 +9,9 @@ export const PublicPageWrapper = ({ children, bgImage }: { children: JSX.Element
       <div className={`vh-100 w-100 __page_wrapper ${bgImage ?? ''}`}>
         <header className='vh-lg-20 w-100'>
           <div className='container h-100 w-100 d-flex justify-content-start align-items-center'>
-            <img src={SpicyGuitarLogo} alt="Spicy Guitar Academy" className='__logo' />
+            <Link to='/welcome'>
+              <img src={SpicyGuitarLogo} alt="Spicy Guitar Academy" className='__logo' />
+            </Link>
           </div>
         </header>
 
