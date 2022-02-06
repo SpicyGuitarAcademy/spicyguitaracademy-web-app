3ab5ec8431075e95c295663d7992495205085fda
Ebuka Odini <ebukaodini@gmail.com>
2022-02-06 17:13:08 +0100

    feat: added the toast wrapper and the toast store

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,31 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { ContactUs, ForgotPassword, LandingPage, LoadingPage, LoginPage, ReadyToPlay, RegisterPage, VerifyAccount, VerifyDevice, WelcomePage } from './pages/public';
 import { WelcomePage as StudentWelcomePage } from './pages/authenticated/index'
+import { ToastWrapper } from './components';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/' children={LandingPage} />
-        <Route exact path='/welcome' children={WelcomePage} />
-        <Route exact path='/loading' children={LoadingPage} />
-        <Route exact path='/register' children={RegisterPage} />
-        <Route exact path='/login' children={LoginPage} />
-        <Route exact path='/verify-device' children={VerifyDevice} />
-        <Route exact path='/verify-account' children={VerifyAccount} />
-        <Route exact path='/forgot-password' children={ForgotPassword} />
-        <Route exact path='/contact-us' children={ContactUs} />
-        <Route exact path='/ready-to-play' children={ReadyToPlay} />
+    <>
+      <ToastWrapper>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={LandingPage} />
+            <Route exact path='/welcome' component={WelcomePage} />
+            <Route exact path='/loading' component={LoadingPage} />
+            <Route exact path='/register' component={RegisterPage} />
+            <Route exact path='/login' component={LoginPage} />
+            <Route exact path='/verify-device' component={VerifyDevice} />
+            <Route exact path='/verify-account' component={VerifyAccount} />
+            <Route exact path='/forgot-password' component={ForgotPassword} />
+            <Route exact path='/contact-us' component={ContactUs} />
+            <Route exact path='/ready-to-play' component={ReadyToPlay} />
 
-        <Route exact path='/welcomee' children={StudentWelcomePage} />
+            <Route exact path='/welcomee' component={StudentWelcomePage} />
 
-      </Switch>
-    </BrowserRouter>
+          </Switch>
+        </BrowserRouter>
+      </ToastWrapper>
+    </>
   );
 }
 
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,1 +1,2 @@
 export { PublicPageWrapper } from './public-page-wrapper/public-page-wrapper'
+export { ToastWrapper } from './toast-wrapper/toast-wrapper'
