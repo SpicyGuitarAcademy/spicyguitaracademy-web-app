83811d2daf23fee6b2ef9bb7522d90b1c88f7e02
Ebuka Odini <ebukaodini@gmail.com>
2022-04-02 23:01:04 +0100

    feat: implemented verify account

--- src/pages/public/verify-account/verify-account.tsx
+++ src/pages/public/verify-account/verify-account.tsx
@@ -1,11 +1,44 @@
 import { PublicPageWrapper } from "../../../components"
 import { AlertCircle } from 'react-feather'
-import { Link } from "react-router-dom"
+import { Link, useHistory, useLocation } from "react-router-dom"
+import { useState } from "react"
+import { useAuthStore } from "../../../store/auth"
+import { useToastStore } from "../../../store/toast"
+import { stateToFormData } from "../../../utils"
+
+type verifyAccountCredentials = {
+  email: string
+  token: string
+}
 
 export const VerifyAccount: React.FC<{}> = () => {
 
+  const { state: locationState } = useLocation<any>()
+  const [credentials, setCredentials] = useState<verifyAccountCredentials>({
+    email: locationState.email,
+    token: ''
+  })
+  const { verifyAccount } = useAuthStore()
+  const { toast } = useToastStore()
+  const { replace } = useHistory()
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
+
+    verifyAccount(stateToFormData(credentials))
+      .then(resp => {
+        console.log(resp)
+
+        if (!resp?.status) {
+          toast(resp?.message, undefined, 'danger')
+        } else {
+          toast(resp?.message)
+
+          replace('/reset-password', {
+            email: credentials.email
+          })
+        }
+      })
   }
 
   return (
@@ -24,7 +57,7 @@ export const VerifyAccount: React.FC<{}> = () => {
                 <div className="mb-lg-4">
 
                   <div className="form-floating mb-lg-3">
-                    <input type="text" maxLength={6} id="authToken" className="form-control mb-lg-2" placeholder="123456" />
+                    <input onChange={(e) => setCredentials({ ...credentials, token: e.target.value })} type="text" maxLength={6} id="authToken" className="form-control mb-lg-2" placeholder="123456" />
                     <label htmlFor="authToken">Authentication Token</label>
                     <small className="text-danger"><AlertCircle size={'18px'} /> If you don't find the mail in your inbox, please check your Spam or Junk folder.</small>
                   </div>
@@ -34,8 +67,10 @@ export const VerifyAccount: React.FC<{}> = () => {
                 <div className="mb-lg-3">
                   <button type="submit" className="btn btn-lg btn-primary form-control mb-3">Verify</button>
                   <div className="d-flex justify-content-between">
-                    <span className="">Didn't get the code? <span className="text-decoration-underline cursor-pointer" onClick={() => { }}>Send again</span></span>
-                    <span className="">Need assistance? <Link to='/contact-us'>Contact us</Link></span>
+                    <span className="">Didn't get the code?<br /><span className="text-decoration-underline cursor-pointer" onClick={() => {
+
+                    }}>Send again</span></span>
+                    <span className="">Need assistance?<br /><Link to='/contact-us'>Contact us</Link></span>
                   </div>
                 </div>
 
