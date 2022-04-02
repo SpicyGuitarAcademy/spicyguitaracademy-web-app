c377ad3f0f83f01479906d533dfd95ac64d69347
Ebuka Odini <ebukaodini@gmail.com>
2022-04-02 23:04:48 +0100

    feat: implemented booting for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { ContactUs, ForgotPassword, LandingPage, LoadingPage, LoginPage, ReadyToPlay, RegisterPage, VerifyAccount, VerifyDevice } from './pages/public';
+import { ContactUs, ForgotPassword, LandingPage, LoginPage, ReadyToPlay, RegisterPage, ResetPassword, VerifyAccount, VerifyDevice } from './pages/public';
 import { WelcomePage as StudentWelcomePage } from './pages/authenticated'
 import { ConfirmModal, LoadingModal, ToastWrapper } from './components';
 import { useLoadingModalStore } from './store/loading-modal';
 import { useEffect } from 'react';
+import { useAuthStore } from './store/auth';
 
 function App() {
 
   const { setLoading } = useLoadingModalStore()
+  const { boot } = useAuthStore()
 
   useEffect(() => {
     setLoading(true, "Booting App...")
-    setTimeout(() => {
-      setLoading(false)
-    }, 2000);
-  }, [setLoading])
+    boot()
+    setLoading(false)
+  }, [setLoading, boot])
 
   return (
     <>
@@ -24,12 +25,12 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={LandingPage} />
-          <Route exact path='/loading' component={LoadingPage} />
           <Route exact path='/register' component={RegisterPage} />
           <Route exact path='/login' component={LoginPage} />
-          <Route exact path='/verify-device' component={VerifyDevice} />
-          <Route exact path='/verify-account' component={VerifyAccount} />
           <Route exact path='/forgot-password' component={ForgotPassword} />
+          <Route exact path='/verify-account' component={VerifyAccount} />
+          <Route exact path='/reset-password' component={ResetPassword} />
+          <Route exact path='/verify-device' component={VerifyDevice} />
           <Route exact path='/contact-us' component={ContactUs} />
           <Route exact path='/ready-to-play' component={ReadyToPlay} />
 
