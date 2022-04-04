48b76ca85fa523b886360537f2b3368de314a2bd
Ebuka Odini <ebukaodini@gmail.com>
2022-04-04 21:06:58 +0100

    feat: implemented verify device and reset device

--- src/pages/public/verify-device/verify-device.tsx
+++ src/pages/public/verify-device/verify-device.tsx
@@ -1,11 +1,40 @@
+import { useState } from "react"
 import { AlertCircle } from "react-feather"
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
 import { PublicPageWrapper } from "../../../components"
+import { useAuthStore } from "../../../store/auth"
+import { useLoadingModalStore } from "../../../store/loading-modal"
+import { useToastStore } from "../../../store/toast"
+import { stateToFormData } from "../../../utils"
+
+type verifyDeviceCredentials = {
+  device: string,
+  token: string
+}
 
 export const VerifyDevice: React.FC<{}> = () => {
 
+  const { deviceInfo, resetDevice, verifyDevice } = useAuthStore()
+  const { setLoading } = useLoadingModalStore()
+  const [credentials, setCredentials] = useState<verifyDeviceCredentials>({
+    device: deviceInfo,
+    token: ''
+  })
+  const { toast } = useToastStore()
+  const { replace } = useHistory()
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
+    setLoading(true)
+
+    resetDevice(stateToFormData(credentials))
+      .then(resp => {
+        setLoading(false)
+
+        toast(resp?.message, undefined, resp.status ? 'success' : 'danger')
+        if (resp.status) replace('/welcome');
+
+      })
   }
 
   return (
@@ -24,7 +53,7 @@ export const VerifyDevice: React.FC<{}> = () => {
                 <div className="mb-lg-4">
 
                   <div className="form-floating mb-lg-3">
-                    <input type="text" maxLength={6} id="authToken" className="form-control mb-lg-2" placeholder="123456" />
+                    <input onChange={(e) => setCredentials({ ...credentials, token: e.target.value })} type="text" maxLength={6} id="authToken" className="form-control mb-lg-2" placeholder="123456" />
                     <label htmlFor="authToken">Authentication Token</label>
                     <small className="text-danger"><AlertCircle size={'18px'} /> If you don't find the mail in your inbox, please check your Spam or Junk folder.</small>
                   </div>
@@ -34,8 +63,12 @@ export const VerifyDevice: React.FC<{}> = () => {
                 <div className="mb-lg-3">
                   <button type="submit" className="btn btn-lg btn-primary form-control mb-3">Verify</button>
                   <div className="d-flex justify-content-between">
-                    <span className="">Didn't get the code? <span className="text-decoration-underline cursor-pointer" onClick={() => { }}>Send again</span></span>
-                    <span className="">Need assistance? <Link to='/contact-us'>Contact us</Link></span>
+                    <span className="">Didn't get the code?<br /><span className="text-decoration-underline cursor-pointer" onClick={async () => {
+                      verifyDevice().then(resp => {
+                        toast(resp?.message)
+                      })
+                    }}>Send again</span></span>
+                    <span className="">Need assistance?<br /><Link to='/contact-us'>Contact us</Link></span>
                   </div>
                 </div>
 
