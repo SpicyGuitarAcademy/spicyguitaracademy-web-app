43d14f15c177a247265de12b9cd59cb79f28e5ce
Ebuka Odini <ebukaodini@gmail.com>
2022-04-04 21:09:25 +0100

    refactor: clean up

--- src/pages/public/index.tsx
+++ src/pages/public/index.tsx
@@ -1,4 +1,3 @@
-// export { WelcomePage } from './landing-page/landing-page'
 export { LandingPage } from './landing-page/landing-page'
 export { LoadingPage } from './loading-page/loading-page'
 export { RegisterPage } from './register-page/register-page'
