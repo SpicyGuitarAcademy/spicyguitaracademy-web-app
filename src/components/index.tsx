7243f3c33c71d3d199a460d557e8273a0d090878
Ebuka Odini <ebukaodini@gmail.com>
2022-04-08 11:53:07 +0100

    feat: added the dashboard wrapper

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 export { PublicPageWrapper } from './public-page-wrapper/public-page-wrapper'
+export { DashboardWrapper } from './dashboard-wrapper/dashboard-wrapper'
 export { ToastWrapper } from './toast-wrapper/toast-wrapper'
 export { ConfirmModal } from './confirm-modal/confirm-modal'
 export { LoadingModal } from './loading-modal/loading-modal'
